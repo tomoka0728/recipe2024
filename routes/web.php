b381f3e653b05bc95c91ad8d2f1ac61b5b3c11a1
tomoka0728 <p.s2.l.epq@gmail.com>
2025-06-05 15:34:39 +0900

    不要ファイル削除

--- routes/web.php
+++ routes/web.php
@@ -74,13 +74,6 @@ Route::middleware(['web'])->group(function () {
 
 
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
